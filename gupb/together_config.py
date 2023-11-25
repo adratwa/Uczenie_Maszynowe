f1b46b4e8466eb46d414b063c3a3aef37aeda09f
adratwa <agatadratwa.99@gmail.com>
2023-11-26 00:43:05 +0100

    Final version of cynamonka v4

--- gupb/together_config.py
+++ gupb/together_config.py
@@ -5,10 +5,6 @@ from gupb.controller import ares
 from gupb.controller import batman
 from gupb.controller import bob
 from gupb.controller import cynamonka
-from gupb.controller import cynamonka_v2
-from gupb.controller import cynamonka_v3
-from gupb.controller import cynamonka_v4
-from gupb.controller import cynamonka_v5
 from gupb.controller import forrest_gump
 from gupb.controller import frog
 from gupb.controller import krombopulos
@@ -23,22 +19,18 @@ from gupb.scripts import arena_generator
 CONFIGURATION = {
     'arenas': arena_generator.generate_arenas(50, arena_generator.random_size_generator()),
     'controllers': [
-        #alpha_gupb.AlphaGUPB("AlphaGUPB"),
+        # alpha_gupb.AlphaGUPB("AlphaGUPB"),
         ancymon.AncymonController("Ancymon"),
         aragorn.AragornController("AragornController"),
-        #ares.AresController("Nike"),
+        # ares.AresController("Nike"),
         bob.FSMBot(),
-        #batman.BatmanHeuristicsController('Batman'),
-        #cynamonka.CynamonkaController("Cynamonka"),
-        #cynamonka_v2.CynamonkaController("Cynamonka2"),
-        cynamonka_v3.CynamonkaController("Cynamonka3"),
-        #cynamonka_v4.CynamonkaController("Cynamonka4"),
-        cynamonka_v5.CynamonkaController("Cynamonka5"),
-        #forrest_gump.ForrestGumpController("Forrest Gump"),
-        #frog.FrogController('Frog'),
+        batman.BatmanHeuristicsController('Batman'),
+        cynamonka.CynamonkaController("Cynamonka"),
+        # forrest_gump.ForrestGumpController("Forrest Gump"),
+        # frog.FrogController('Frog'),
         krombopulos.KrombopulosMichaelController(),
         maly_konik.MalyKonik("LittlePonny"),
-        #mongolek.Mongolek('Mongolek'),
+        # mongolek.Mongolek('Mongolek'),
         pat_i_kot.PatIKotController("Kot i Pat"),
         random.RandomController("Alice"),
         r2d2.RecklessRoamingDancingDruid("R2D2"),
